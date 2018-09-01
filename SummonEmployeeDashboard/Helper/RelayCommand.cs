2b14a4824e8b067433aeaa87f89fe3819304627e
Ernest Asanov <asanovernest23@gmail.com>
2018-09-01 19:25:02 +0300

    new statistics with DataGrid

--- SummonEmployeeDashboard/Helper/RelayCommand.cs
+++ SummonEmployeeDashboard/Helper/RelayCommand.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.Windows.Input;
 
-namespace SummonEmployeeDashboard.ViewModels
+namespace SummonEmployeeDashboard
 {
     /// <summary>
     /// A command whose sole purpose is to 
